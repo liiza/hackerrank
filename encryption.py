05870cc2917d3dc4486a3018ba9dae9d70549c27
liisa Sailaranta <liisa.sailaranta>
2017-12-15 20:28:07 -0800

    Added assignment Encryption

---
+++ encryption.py
@@ -0,0 +1,19 @@
+import sys
+import math
+
+s = input().strip()
+l = len(s)
+
+floor = math.floor(l)
+ceil = math.ceil(l)
+
+row = min(floor, ceil)
+column = max(floor, ceil)
+
+encrypted = [""] * row
+
+for index, c in enumerate(s):
+     encrypted[index%row] += c
+
+
+print(" ".join(encrypted))
