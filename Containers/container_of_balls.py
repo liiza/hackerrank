9e976b5a3f6077cb34979539bd2a35d940050d45
liisa Sailaranta <liisa.sailaranta>
2017-12-15 23:25:03 -0800

    Added Container puzzle.

---
+++ Containers/container_of_balls.py
@@ -0,0 +1,26 @@
+import sys
+
+
+q = int(input().strip())
+for a0 in range(q):
+	n = int(input().strip())
+	M = []
+	for M_i in range(n):
+	   M_t = [int(M_temp) for M_temp in input().strip().split(' ')]
+	   M.append(M_t)
+
+	container_lens = []
+	balls_lens = [0] * n
+	for container in M:
+		container_lens.append(len(container))
+		for index, balls in enumerate(container):
+			balls_lens[index] += balls
+
+	print(sorted(container_lens))
+	print(sorted(balls_lens))
+	if sorted(container_lens) == sorted(balls_lens):
+		print('Possible')
+	else:
+		print('Impossible')
+
+
