18725fe5c910b1744ae6285d52bba359984d69f5
liisa Sailaranta <liisa.sailaranta>
2017-12-18 09:12:39 -0800

    Add almost sorted.

---
+++ AlmostSorted/almost_sorted.py
@@ -0,0 +1,59 @@
+# https://www.hackerrank.com/challenges/almost-sorted/problem
+_ = input()
+nums = [int(i) for i in input().split(' ')]
+
+holes = []
+hills = []
+
+for i in range(1, len(nums) - 1):
+    if nums[i - 1] > nums[i] < nums[i + 1]:
+        holes.append(i)
+    elif nums[i - 1] < nums[i] > nums[i + 1]:
+        hills.append(i)
+        
+if nums[0] > nums[1]:
+    hills.append(0)
+if nums[-1] < nums[-2]:
+    holes.append(len(nums) - 1)
+
+if len(hills) == 1 and len(holes) == 1:
+    if (
+            (
+                (holes[0] + 1 == len(nums)) or (nums[hills[0]] < nums[holes[0] + 1])
+            )
+            and 
+            (
+                (hills[0] == 0) or (nums[holes[0]] > nums[hills[0] - 1])
+            )
+        ):
+        print('yes')
+        if holes[0] - hills[0] == 1:
+            print('swap', hills[0] + 1, holes[0] + 1)
+        else:
+            print('reverse', hills[0] + 1, holes[0] + 1)
+    else:
+        print('no')
+elif len(hills) == 2 and len(holes) == 2:
+    if (
+        (
+            (holes[1] + 1 == len(nums)) or (nums[hills[0]] < nums[holes[1] + 1])
+        )
+        and 
+        (
+            (holes[1] == 0) or (nums[hills[0]] > nums[holes[1] - 1])
+        )
+        and 
+        (
+            (hills[0] + 1 == len(nums)) or (nums[holes[1]] < nums[hills[0] + 1])
+        )
+        and 
+        (
+            (hills[0] == 0) or (nums[holes[1]] > nums[hills[0] - 1])
+        )
+    ):
+        print('yes')
+        print('swap', hills[0] + 1, holes[1] + 1)
+    else:
+        print('no')
+else:
+    print('no')
